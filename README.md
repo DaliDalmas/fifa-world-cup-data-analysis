c921719eb4297abd0f8c7969e39f99562c56442b
Dalmas.Otieno <dalmas.otieno@GLB-1602019-1779.local>
2022-12-25 02:08:54 +0300

    restructure

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # fifa-world-cup-data-analysis
 
 This project automates the scraping of fifa world cup data data using selenium and storing that data in a postgres database. The database is configured and hosted in a docket container. The data is then used to build interactive analytics dashboards using both jupyter notebooks and Dash.
+
+# Learning
+
+By the begining of this project I did not have any technical knowledge using selenium. I tried winging the project but I could not pull it through. I therefore decided to learn selenium first before attampting the project. All my practise files are in the learning_selenium folder.
--- learning_selenium/intro.py
+++ learning_selenium/intro.py
@@ -1,7 +1,15 @@
 # chrome driver site https://sites.google.com/chromium.org/driver/?pli=1
 # xattr -d com.apple.quarantine chromedriver 
+# code source https://scrapfly.io/blog/web-scraping-with-selenium-and-python/
+# onetrust-accept-btn-handler
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+# from parsel import Selector
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 options = Options()
 options.add_experimental_option("detach", True) # allow the window to remain open
@@ -21,3 +29,17 @@ driver = webdriver.Chrome(PATH, options=options)
 URL = 'https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022/scores-fixtures?country=UG&wtw-filter=ALL'
 print(driver.title)
 driver.get(URL)
+
+print(1)
+# wait for page to load
+element = WebDriverWait(driver=driver, timeout=60)
+# .until(
+#     EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-target=directory-first-item]'))
+# )
+print(2)
+ACCEPT_COOKIES_BUTTON_XPATH = '//button[@id="onetrust-accept-btn-handler"]'
+button = driver.find_element_by_xpath(ACCEPT_COOKIES_BUTTON_XPATH)
+print(3)
+button.click()
+print(4)
+print(driver.page_source)
